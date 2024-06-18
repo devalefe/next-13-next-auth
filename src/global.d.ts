864307dafb0d9f15bb1d4ded3305856331f82418
Alefe <alefefoliveira@gmail.com>
2024-06-18 16:11:12 -0300

    fix: improve user data is required

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,10 @@ import { DefaultUser } from "next-auth";
 interface CustomUser extends DefaultUser {
   // aqui vc adiciona os dados que vc precisa do user
   // assim vc pode usá-los na aplicação sem erro de tipagem
+  id: string;
+  name: string;
+  email: string;
+  image: string;
 }
 
 declare module "next-auth" {
