4ab1545884a93f459148eb095865fd6a36aaa27a
Alefe <alefefoliveira@gmail.com>
2024-06-18 16:11:42 -0300

    feature: add user info in header

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { useSession, signOut } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { status } = useSession();
-
-  if (status === "unauthenticated") {
-    return redirect("/signin");
-  }
+  const { data: session, status } = useSession();
 
   if (status === "loading") {
     return (
@@ -18,9 +15,35 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     );
   }
 
+  if (status === "unauthenticated" || !session || !session.user) {
+    return redirect("/signin");
+  }
+
   return (
     <div className="flex flex-1 flex-col">
-      <div className="flex h-16 bg-blue-400 p-3">Header</div>
+      <div className="flex h-16 items-center justify-between gap-3 bg-gray-50 px-3 py-2 shadow-xl">
+        <div className="flex items-center gap-3">
+          <Image
+            src={session.user.image}
+            alt="User Avatar"
+            height={48}
+            width={48}
+            className="rounded-full"
+          />
+
+          <div className="flex flex-col gap-1">
+            <span className="text-sm font-bold">{session.user.name}</span>
+            <span className="text-xs text-gray-600">{session.user.email}</span>
+          </div>
+        </div>
+
+        <button
+          onClick={() => signOut()}
+          className="rounded bg-black/90 px-3 py-1 text-gray-300 hover:bg-black hover:text-gray-50"
+        >
+          Sair
+        </button>
+      </div>
       {children}
     </div>
   );
