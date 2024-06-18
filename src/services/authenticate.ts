80fac04d9de3e001244add901e36893c2f869095
Alefe <alefefoliveira@gmail.com>
2024-06-18 15:41:05 -0300

    cleanup: remove unused data rerurned by service/authenticate

--- src/services/authenticate.ts
+++ src/services/authenticate.ts
@@ -14,17 +14,15 @@ export async function autenticate(params: Params): Promise<CustomUser | null> {
       JSON.stringify({
         status: 401,
         error: "Credenciais inválidas",
-      })
+      }),
     );
   }
 
   // simulando uma resposta de sucesso
   return {
     id: "1",
-    avatar: "https://www.github.com/devalefe.png",
+    image: "https://www.github.com/devalefe.png",
     name: "Fulano de Tal",
     email: "valid@email.com",
-    accessToken: "access-token",
-    refreshToken: "refresh-token",
   };
 }
