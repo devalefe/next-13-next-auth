b34edc274a105c3220c782aa3e43a59913fc069a
Alefe <alefefoliveira@gmail.com>
2024-06-18 16:10:52 -0300

    feature: add github doamin to avatar support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "avatars.githubusercontent.com",
-      "vercel.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.github.com",
+      },
     ],
   },
   experimental: {
