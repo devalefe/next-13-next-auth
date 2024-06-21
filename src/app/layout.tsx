0ea42b69308096ef3742d3fcb9bc4c098d7d42ec
Alefe <alefefoliveira@gmail.com>
2024-06-21 15:53:24 -0300

    chore: chage app font from Inter to Poppins, set user-select to none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { Providers } from "@/providers/session-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Authentication Example",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={`${inter.className} flex`}>
+      <body className={`${poppins.className} flex`}>
         <div className="flex flex-1 flex-col">
           <Providers>{children}</Providers>
         </div>
